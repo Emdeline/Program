1f870dbafd8b519b2f7066d0439a6c05bf44a175
emdeline <emdeline@10.0.0.154>
2019-10-21 18:23:18 -0600

    Finished Project

--- Program/src/ProgramRunner.java
+++ Program/src/ProgramRunner.java
@@ -1,96 +1,88 @@
 import java.util.ArrayList;
 
-public class ProgramRunner {
-	
-	public static void printList()
-		{
-		
-		}
-	public static void plusOneSeason()
-		{
-		
-		}
-	public static void takeOutLongestTitle()
-		{
-		
-		}
+public class ProgramRunner 
 
+{
+	
+	static ArrayList <Program> TVShows = new ArrayList <Program> ();
+	
 	public static void main(String[] args) 
 		{
-//Question 5-8 code
-		ArrayList <Program> TVShows = new ArrayList <Program> ();
+			arrayMethod();
+			printTVShows();
+			plusOneSeason();
+			takeOutLongestTitle();
+			addTitle();
+		}
+	
+	public static void arrayMethod()
+		{
 				TVShows.add(new Program ("Teen Titans" , "Action" , 5));
 				TVShows.add(new Program ("Avatar" , "Action" , 3));
 				TVShows.add(new Program ("Stanger Things" , "Adventer/Thriller" , 3));
 				TVShows.add(new Program ("Hunting of Hill House" , "Horror" , 1));
 				TVShows.add(new Program ("Big Mouth" , "Scaring" , 2));
-		
-			for (Program t : TVShows)
-			{
-				System.out.println(t.getTitle());
-				System.out.println(t.getGenre());
-				System.out.println(t.getNumberOfSeasonsAired());
-				System.out.println(" ");
-				
-			}
-			printList();
-			
-//Question 9 code 
-			plusOneSeason();
-		{
-			ArrayList <Program> TVShows1 = new ArrayList <Program> ();
-				TVShows1.add(new Program ("Teen Titans" , "Action" , 5));
-				TVShows1.add(new Program ("Avatar" , "Action" , 3));
-				TVShows1.add(new Program ("Stanger Things" , "Adventer/Thriller" , 3));
-				TVShows1.add(new Program ("Hunting of Hill House" , "Horror" , 1));
-				TVShows1.add(new Program ("Big Mouth" , "Scaring" , 2));
-
-	for (Program t : TVShows1)//
-		{
-			System.out.println(t.getTitle());
-			System.out.println(t.getGenre());
-			System.out.println(t.getNumberOfSeasonsAired() + 1);
-			System.out.println(" ");
-
 		}
-			printList();
-			
-//Question 10 code
-			takeOutLongestTitle();
-			ArrayList <Program> TVShows2 = new ArrayList <Program> ();
-			TVShows2.add(new Program ("Teen Titans" , "Action" , 5));
-			TVShows2.add(new Program ("Avatar" , "Action" , 3));
-			TVShows2.add(new Program ("Stanger Things" , "Adventer/Thriller" , 3));
-			TVShows2.add(new Program ("Big Mouth" , "Scaring" , 2));
 	
-		for (Program t : TVShows2)
-			{
-				System.out.println(t.getTitle());
-				System.out.println(t.getGenre());
-				System.out.println(t.getNumberOfSeasonsAired());
-				System.out.println(" ");
-				
-			}
-		printList();
-		}
-
-//Question 11 code
-	ArrayList <Program> TVShows3 = new ArrayList <Program> ();
-		TVShows3.add(new Program ("Twelve Forever" , "Fantasy" , 1));
-		TVShows3.add(new Program ("Avatar" , "Action" , 3));
-		TVShows3.add(new Program ("Stanger Things" , "Adventer/Thriller" , 3));
-		TVShows3.add(new Program ("Hunting of Hill House" , "Horror" , 1));
-		TVShows3.add(new Program ("Big Mouth" , "Scaring" , 2));
-
-	for (Program t : TVShows3)
+	public static void printTVShows()
 		{
-			System.out.println(t.getTitle());
-			System.out.println(t.getGenre());
-			System.out.println(t.getNumberOfSeasonsAired());
+			for (Program t : TVShows)
+				{
+					System.out.println(t.getTitle() + " " + t.getGenre() + " " + t.getNumberOfSeasonsAired());
+			
+				}
 			System.out.println(" ");
-		
 		}
-	printList();
+	
+	public static void plusOneSeason()
+		{
+			for (Program t : TVShows)
+				{
+					System.out.print(t.getTitle() + " " + t.getGenre() + " ");
+					System.out.print(t.getNumberOfSeasonsAired() + 1);
+					System.out.println();
+
+				}
+			System.out.println(" ");
 
 		}
+	
+	public static void takeOutLongestTitle()
+		{
+			int max = TVShows.get(0).getTitle().length();
+			int index = 0;
+			
+				for (int t = 0; t < TVShows.size(); t++)
+					{
+						if (TVShows.get(t).getTitle().length() > max)
+							{
+								index = t; 
+							}
+					}
+				TVShows.remove(index);
+				
+				for (Program t : TVShows)
+					{
+						System.out.println(t.getTitle() + " " + t.getGenre() + " " + t.getNumberOfSeasonsAired());
+					}
+				System.out.println(" ");
+		}
+	
+	public static void addTitle()
+		{
+			TVShows.get(0).setTitle("Amazing World of Gumball");
+			
+				for (Program t : TVShows)
+					{
+						System.out.println(t.getTitle() + " " + t.getGenre() + " " + t.getNumberOfSeasonsAired());
+						
+					}
+				System.out.println(" ");
+		}
+		
 }
+
+
+
+		
+
